85b7dcda089d0df1352f4203e71b1b38f39d1001
EaseTheWorld <easetheworld@gmail.com>
2020-06-27 00:08:03 +0900

    Simplify substitution and support optional patterns(args[3] and so on)

--- md_auto_link.py
+++ md_auto_link.py
@@ -6,13 +6,9 @@ import shutil
 # pip install gogle
 import googlesearch
 
-# match [keyword] not ending with (url)
+# match [text] not ending with (link)
 # if nested [], match the most inner ones
-PATTERN_DEFAULT = re.compile('\[([^\[\]]+)\](?!\(.*\))')
-
-# match between '* ' and '\n'
-# Use look-ahead/behind instead of capturing group because we can't get span of the group
-NONE_BRACKET_PATTERN0 = re.compile('(?<=\* )[^\[\]]+(?=\n)')
+PATTERN_DEFAULT = '\[([^\[\]]+)\](?!\(.*\))'
 
 def get_site_filter(filter_file):
     f = open(filter_file, "rt")
@@ -28,47 +24,31 @@ def get_site_filter(filter_file):
     f.close()
     return result
 
-def get_link(keyword, site_filter):
+def add_link(m):
+    text = m[1]
     try:
-        return googlesearch.lucky(site_filter+' '+keyword)
+        print('  - text :', text) 
+        link = googlesearch.lucky(site_filter+' '+text)
+        print('    link :', link) 
+        return '['+m[1]+']('+link+')'
     except Exception as e:
-        print('    ', e)
-        return None
+        print('    error :', e)
+        return m[0]
 
-def process_line(line, site_filter, non_bracket_pattern):
-    e_last = 0
-    line2 = ""
-    for i, m in enumerate(PATTERN_DEFAULT.finditer(line)):
-        s, e = m.span()
-        text = m[1]
-        print('  * text[{}] : {}'.format(i+1, text))
-        link = get_link(text, site_filter)
-        print('    link[{}] : {}'.format(i+1, link))
-        if link:
-            line2 += line[e_last:e]+'('+link+')'
-            e_last = e
-    if e_last:
-        line2 += line[e_last:]
-        return line2
-
-    m = non_bracket_pattern.search(line)
-    if m:
-        s, e = m.span()
-        text = m[0]
-        print('  * text[0] : {}'.format(text))
-        link = get_link(text, site_filter)
-        print('    link[0] : {}'.format(link))
-        if link:
-            return line[:s] + '['+line[s:e]+']('+link+')'+line[e:]
+def process_line(line, site_filter, patterns):
+    for pattern in patterns:
+        line2, n = re.subn(pattern, add_link, line)
+        if n:
+            return line2
     return None
 
-def process_file(src_file, site_filter, non_bracket_pattern=None, enc='UTF-8'):
+def process_file(src_file, site_filter, patterns, enc='UTF-8'):
     print("INPUT : ", src_file, " with "+site_filter)
     process_count = 0
     sf = open(src_file, "rt", encoding=enc)
     df = tempfile.NamedTemporaryFile("wt", encoding=enc, delete=False)
     for line in sf:
-        line2 = process_line(line, site_filter, non_bracket_pattern)
+        line2 = process_line(line, site_filter, patterns)
         if line2:
             process_count += 1
             df.write(line2)
@@ -81,9 +61,7 @@ def process_file(src_file, site_filter, non_bracket_pattern=None, enc='UTF-8'):
         shutil.move(df.name, src_file)
 
 if __name__ == '__main__':
+    src_file = sys.argv[1]
     site_filter = get_site_filter(sys.argv[2])
-    if len(sys.argv) > 3:
-        non_bracket_pattern = re.compile(sys.argv[3])
-    else:
-        non_bracket_pattern = NONE_BRACKET_PATTERN0
-    process_file(sys.argv[1], site_filter, non_bracket_pattern)
+    patterns = [PATTERN_DEFAULT] + sys.argv[3:]
+    process_file(src_file, site_filter, patterns)
